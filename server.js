2917e48c417a86a59a711eab9bb145d375a3395d
Raktim Maity <108602390+Raktimmaity@users.noreply.github.com>
2025-07-01 18:39:12 +0530

    update server.js file

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://raphaaa.onrender.com", // change to your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 dotenv.config();
 
