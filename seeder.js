ac568498458bc6dd9bf13cd1b96b427506c81cda
Raktim Maity <108602390+Raktimmaity@users.noreply.github.com>
2025-07-04 16:53:20 +0530

    update seed.js

--- seeder.js
+++ seeder.js
@@ -1,49 +1,49 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const Product = require("./models/Product");
-const User = require("./models/User");
-const Cart = require("./models/Cart");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const Product = require("./models/Product");
+// const User = require("./models/User");
+// const Cart = require("./models/Cart");
 
-const products = require("./data/products");
+// const products = require("./data/products");
 
-dotenv.config();
+// dotenv.config();
 
-// Connect to the mongoDB database
-mongoose.connect(process.env.MONGO_URI);
+// // Connect to the mongoDB database
+// mongoose.connect(process.env.MONGO_URI);
 
-// Function to seed data
+// // Function to seed data
 
-const seedData = async () => {
-    try {
-        // Clear existing data
-        await Product.deleteMany();
-        await User.deleteMany();
-        await Cart.deleteMany();
+// const seedData = async () => {
+//     try {
+//         // Clear existing data
+//         await Product.deleteMany();
+//         await User.deleteMany();
+//         await Cart.deleteMany();
 
-        // Create a default admin User
-        const createdUser = await User.create({
-                name: "Admin User",
-                email: "admin@admin.com",
-                password: "nopass",
-                role: "admin",
-            });
+//         // Create a default admin User
+//         const createdUser = await User.create({
+//                 name: "Admin User",
+//                 email: "admin@raphaaa.com",
+//                 password: "nopass",
+//                 role: "admin",
+//             });
            
-        // Assign the default user ID to each product
-        const userID = createdUser._id;
+//         // Assign the default user ID to each product
+//         const userID = createdUser._id;
 
-        const sampleProducts = products.map((product) => {
-            return { ...product, user: userID };
-        });
+//         const sampleProducts = products.map((product) => {
+//             return { ...product, user: userID };
+//         });
 
-        // Insert the products into the database
-        await Product.insertMany(sampleProducts);
+//         // Insert the products into the database
+//         await Product.insertMany(sampleProducts);
 
-        console.log("Product data seeded successfully!!");
-        process.exit();
-    } catch (error) {
-        console.error("Error seeding the data:", error );
-        process.exit(1);
-    }
-};
+//         console.log("Product data seeded successfully!!");
+//         process.exit();
+//     } catch (error) {
+//         console.error("Error seeding the data:", error );
+//         process.exit(1);
+//     }
+// };
 
-seedData();
+// seedData();
