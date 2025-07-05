0de8133ced87fcd402bf6e58d6172f310b51531a
Raktim Maity <108602390+Raktimmaity@users.noreply.github.com>
2025-07-05 15:09:58 +0530

    update server.js package.json

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const serverless = require("serverless-http");
-
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
@@ -16,22 +14,31 @@ const productAdminRoutes = require("./routes/productAdminRoutes");
 const adminOrderRoutes = require("./routes/adminOrderRoutes");
 const merchRoutes = require("./routes/merchRoutes");
 
-dotenv.config();
-connectDB();
+const corsConfig = {
+    origin: "*",
+    Credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"] ,
+};
 
 const app = express();
 app.use(express.json());
-
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-};
+// app.use(cors({
+//   origin: "http://localhost:9000", // change to your frontend URL
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }));
+app.options("", cors(corsConfig));
 app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to the MongoDB database
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Raphaaa API!!");
+    res.send("Welcome tp Raphaaa API!!");
 });
 
 // API Routes 
@@ -42,11 +49,13 @@ app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api", subscriberRoutes);
+
+// Admin routes
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 app.use("/api/merch", merchRoutes);
 
-// ❌ REMOVE app.listen()
-// ✅ Export as serverless function
-module.exports = serverless(app);
+app.listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
+});
